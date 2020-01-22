73e9fe6fa4ff4d661b2cae68cdd4c2025eb908e9
hholo <hholoron@gmail.com>
2020-01-22 22:41:52 +0300

    Header and main

--- Script/Script.js
+++ Script/Script.js
@@ -8,4 +8,19 @@ document.addEventListener('DOMContentLoaded',function(){
             clickable: true,
         },
     });
+    window.onclick = function (event) {
+        for (var i = 0; i < 7; i++) {
+            for (var j = 0; j < 7; j++) {
+                if (event.target == document.getElementsByName("menu")[i]|| event.target == document.getElementsByClassName("menutext")[i] ) {
+                    document.getElementsByName("menu")[j].style.backgroundColor = "#03499E";
+                    document.getElementsByClassName("menutext")[j].style.color = "white";
+                }
+            }
+            if (event.target == document.getElementsByName("menu")[i] || event.target == document.getElementsByClassName("menutext")[i]) {
+                document.getElementsByName("menu")[i].style.backgroundColor = "#F7F7F7";
+                document.getElementsByClassName("menutext")[i].style.color = "#129FDF";
+            }
+        }
+    }
+
 });
