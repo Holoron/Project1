df15e53ed43b8fc1febf9c86f22d24d089236b73
Holoron <56965835+Holoron@users.noreply.github.com>
2020-01-30 15:47:38 +0300

    Header,Main,Footer,InternalV.0.2

--- Script/Script.js
+++ Script/Script.js
@@ -1,9 +1,11 @@
-document.addEventListener('DOMContentLoaded',function(){
-        var swiper = new Swiper('.TabSwiper>.swiper-container', {
+document.addEventListener('DOMContentLoaded',function() {
+    var swiper = new Swiper('.TabSwiper>.swiper-container', {
         slidesPerView: 4.43,
         spaceBetween: 2,
         freeMode: true,
+
     });
+
 //
 //     var swiper = new Swiper('.box_score_slider .swiper-container', {
 //         effect: 'coverflow',
@@ -23,31 +25,58 @@ document.addEventListener('DOMContentLoaded',function(){
 //             prevEl: '.box_score_slider .swiper-button-prev',
 //         },
 //     });
-// }
-    window.onclick = function (event) {
-        for (var i = 0; i < 7; i++) {
-            for (var j = 0; j < 7; j++) {
-                if (event.target == document.getElementsByName("menu")[i]|| event.target == document.getElementsByClassName("menutext")[i] ) {
-                    document.getElementsByName("menu")[j].style.backgroundColor = "#03499E";
-                    document.getElementsByClassName("menutext")[j].style.color = "white";
-                }
-            }
-            if (event.target == document.getElementsByName("menu")[i] || event.target == document.getElementsByClassName("menutext")[i]) {
-                document.getElementsByName("menu")[i].style.backgroundColor = "#F7F7F7";
-                document.getElementsByClassName("menutext")[i].style.color = "#129FDF";
-            }
-        }
-    }
-
 });
+window.onclick = function (event) {
+    if(event.target==document.getElementById("search"))
+    {
+        document.getElementsByClassName("list")[0].style.display="none";
+        document.getElementsByClassName("search")[0].style.display="block";
+    }
+    if(event.target==document.getElementById("searchsecond"))
+    {
+        document.getElementsByClassName("search")[0].style.display="none";
+        document.getElementsByClassName("list")[0].style.display="flex";
+    }
+}
 
 
 function email() {
     var r=/^\w+@\w+\.\w{2,4}$/i;
-    if(r.test(document.getElementById("").value))
+    if(!r.test(document.getElementById("email").value))
     {
-        document.getElementById("").style.outline="red";
-        document.getElementsByClassName("")[4].style.color="red";
+        document.getElementById("email").style.borderBottom=" 1px solid red";
+        document.getElementById("email").style.borderTop=" 1px solid red";
+        document.getElementById("email").style.borderLeft=" 1px solid red";
+        document.getElementById("emaillabel").style.color="red";
+        document.getElementById("email").focus();
     }
-
+    else
+    {
+        document.getElementById("email").style.border="none";
+        document.getElementById("emaillabel").style.color="black";
+    }
+}
+function find() {
+    for (var i=0;i<5;i++)
+    {
+        if(document.getElementsByClassName("inp")[i].value.trim()=="")
+        {
+            document.getElementsByClassName("label")[i].style.color="red";
+            document.getElementsByClassName("label")[i].textContent="Неправильный формат ввода";
+            document.getElementsByClassName("inp")[i].style.border="1px solid red";
+        }
+        else
+        {
+            document.getElementsByClassName("inp")[i].style.border="none";
+            document.getElementsByClassName("label")[i].style.color="black";
+            switch (i)
+            {
+                case 0:document.getElementsByClassName("label")[i].textContent="Откуда";break;
+                case 1:document.getElementsByClassName("label")[i].textContent="Куда";break;
+                case 2:document.getElementsByClassName("label")[i].textContent="Дата вылета";break;
+                case 3:document.getElementsByClassName("label")[i].textContent="Ночей";break;
+                case 4:document.getElementsByClassName("label")[i].textContent="Туристы";break;
+            }
+        }
+    }
 }
